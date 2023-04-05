c6c520cb83ab264800ad401225779189c87acc8c
lokmane101 <lokmaneakkouh10@gmail.com>
2023-04-05 20:29:25 +0100

    luncher is finally ended

--- Account.py
+++ Account.py
@@ -1,12 +1,40 @@
 from tkinter import *
-
+import mysql.connector 
 account=Tk()
 account.attributes('-fullscreen',True)
-iconsbarr=Frame(account,width=240,height=1440,bg="black",)
+iconsbarr=Frame(account,width=150,height=1440,bg="#15b4ea",)
 iconsbarr.place(x=0,y=0)
-# personal_icon=
 
 
+#------------------------------------------partie SQL-------------------------------------------------------------------------------------------------------#
+
+#------------------------connect to the database------------------------------------    
+database = mysql.connector.connect(host='localhost',
+                                database='projet',
+                                user='root',
+                                password='lokmane-SQL-12')
+#------------------------create cursor---------------------------------------------
+cursorr=database.cursor()
+
+#-----------------------------import icons----------------------------------------
+time_icon=PhotoImage(file=r"C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\time1.png")
+time_icon_button=Button(iconsbarr,bg="#15b4ea",padx=0,pady=0, relief="flat",image=time_icon,activebackground="#15b4ea")
+time_icon_button.place(x=15,y=80)
+
+
+
+paper=PhotoImage(file=r"C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\paper1.png")
+paper_button=Button(iconsbarr,bg="#15b4ea",padx=0,pady=0, relief="flat",image=paper,activebackground="#15b4ea")
+paper_button.place(x=25,y=200)
+
+
+icon=PhotoImage(file=r"C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\support.png")
+button=Button(iconsbarr,image=icon,padx=0,pady=0,relief="flat",bg="#15b4ea",activebackground="#15b4ea")
+button.place(x=25,y=780)
+
+person=PhotoImage(file=r"C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\person_icon1.png")
+person_button=Button(iconsbarr,image=person,padx=0,pady=0,relief="flat",bg="#15b4ea",activebackground="#15b4ea")
+person_button.place(x=18,y=300)
 #--------------------------leave button-------------------------------------------
 leavebutton=Button(account,text="Leave",command=account.quit,bg="#258EF5",fg="white",activebackground="#258EF5", activeforeground="white",font=("Arial",16),padx=0,pady=0, relief="flat")      #
 leavebutton.place(x=1440, y=800)
--- Luncher.py
+++ Luncher.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import subprocess
+import mysql.connector 
+
+
 
 
 #---------------------------------------definition des commands des buttons------------------------
@@ -14,19 +17,48 @@ def window_button():
 def forgoten_password():
     subprocess.run(["python", "C:\\Users\\lokmane\\Desktop\\Tkinter_project\\forgottenPwd.py"])
 
-def pass_to_account():
-    subprocess.run(["python","C:\\Users\\lokmane\\Desktop\\Tkinter_project\\Account.py"])
-#---------------------------------------------------------------------------------------------------
+
+def pass_account():
+#----------------get info from name and password fields-------------------------------------
+    nom_text=nomfield.get()
+    password_text=passwordfield.get()
+#------------------execute command-----------------------------------------------------
+    cursorr.execute("SELECT password from Etudiant where CIN='"+nom_text+"';")
+#-------------------fetch result--------------------------------------------------
+    result=cursorr.fetchone()
+#----------------------------------------------------------------------------------
+    authentification=result[0] if result else None #since result of fetch is a tuple we have to selct first element to get the result
+                                                   #i added that if in case the result of fetch was None to not give an error
+    
+    if authentification==password_text:
+        print("script runned")
+        subprocess.run(["python","C:\\Users\\lokmane\\Desktop\\Tkinter_project\\Account.py"])
+        fichier_log=open("fichierLog.txt",'a+')
+        fichier_log.write(nomfield.get()+"\n")
+    else:
+        erreur=Label(window,text="nom d'utilisateur ou mot de passe incorrect",font=("Arial",20),bg="white",fg="red")
+        erreur.place(x=850,y=500)
+
+    
 
 
 
 
 
 
+#------------------------------------------partie SQL-------------------------------------------------------------------------------------------------------#
+
+#------------------------connect to the database------------------------------------    
+database = mysql.connector.connect(host='localhost',
+                                database='projet',
+                                user='root',
+                                password='lokmane-SQL-12')
+#------------------------create cursor---------------------------------------------
+cursorr=database.cursor()
 
 
 
-
+##########################################################################################################################################
 
 
 
@@ -36,12 +68,12 @@ window = Tk()
 
 # Define the geometry of the window (full screen)-------------------------------
 
-window.attributes('-fullscreen',True)
+window.geometry("1200x720")
 #-------------define sceen color----------------------------------------------
 window.config(bg="white")
 #-------------import picture---------------------------------------------------
-imagee=(Image.open("C:\\Users\\lokmane\\Desktop\\Tkinter_project\\school.jpg"))
-imagee=imagee.resize((600,900))
+imagee=(Image.open("C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\school.jpg"))
+imagee=imagee.resize((430,800))
 sidepicture = ImageTk.PhotoImage(imagee)
 #----------------Create a Label Widget to display the  Image-----------------
 label = Label(window, image = sidepicture,padx=0,pady=0, relief="flat")
@@ -53,42 +85,47 @@ label.grid(row=0,column=0)
 # activebackground is color when to kpress on button ############ activeforegroud is color of text when press  #
 # bg is color of background ##################################### fg is color of font                          #
 #--------------------------------------------------------------------------------------------------------------#
-
+lock_icon=Image.open("C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\lock_icon.png")
+lock_icon=lock_icon.resize((40, 40), Image.ANTIALIAS)
+lock_icon=ImageTk.PhotoImage(lock_icon)
+person_icon=Image.open("C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\person_icon.png")
+person_icon=person_icon.resize((50, 50), Image.ANTIALIAS)
+person_icon=ImageTk.PhotoImage(person_icon)
 
 
 
 #----------------------------welcome label--------------------------------------------
-welcome=Label(window,text="ESPACE     ETUDIANT ",font=("Arial",65),bg="white")
-welcome.place(x=605,y=0)
+welcome=Label(window,text="ESPACE     ETUDIANT ",font=("Arial",54),bg="white")
+welcome.place(x=435,y=0)
 
 #---------------------------nom label and entry field--------------------------------
-nom=Label(window,text="User Name:",font=("Arial",40),bg="white")
-nom.place(x=950,y=150)
+nom=Label(window,text="Nom d'utilisateur:",font=("Arial",35),bg="white",image=person_icon,compound="left")
+nom.place(x=600,y=150)
 
-nomfield=Entry(window,width=25,font=("Arial",30),relief="flat",bg="#e1f3ff")
-nomfield.place(x=850,y=250)
+nomfield=Entry(window,width=20,font=("Arial",30),relief="flat",bg="#e1f3ff")
+nomfield.place(x=580,y=220)
 #--------------------------password label and entry field----------------------------
-password=Label(window,text="mot de passe:",font=("Arial",40),bg="white")
-password.place(x=950,y=350)
+password=Label(window,text="Mot de passe:",font=("Arial",35),bg="white",image=lock_icon,compound="left")
+password.place(x=600,y=280)
 
-passwordfield=Entry(window,width=25,font=("Arial",30),relief="flat",bg="#e1f3ff")
-passwordfield.place(x=850,y=450)
+passwordfield=Entry(window,width=20,font=("Arial",30),relief="flat",bg="#e1f3ff")
+passwordfield.place(x=580,y=340)
 #-------------------confirm button---------------------------------------------------
 
-confirm_button=Button(window,text="confirm",command=pass_to_account, bg="#258EF5",fg="white",activebackground="#258EF5", activeforeground="white",font=("Arial",20),padx=0,pady=0, relief="flat",width=35)
-confirm_button.place(x=850,y=550)
+confirm_button=Button(window,text="confirm",command=pass_account, bg="#258EF5",fg="white",activebackground="#258EF5", activeforeground="white",font=("Arial",20),padx=0,pady=0, relief="flat",width=35)
+confirm_button.place(x=850,y=580)
 
 #-------------------------forgot password -------------------------------------------
 
 
 forgot_password=Button(window,text=("password forgoten"),command=forgoten_password,bg="white",fg="#258EF5",activebackground="white", activeforeground="#258EF5",font=("Arial",14),padx=0,pady=0, relief="flat")
-forgot_password.place(x=850,y=620)
+forgot_password.place(x=850,y=650)
 
 
 #--------------------------sing up buttom--------------------------------------------
 
 signUp=Button(window,text=("sign up"),command=signup_button,bg="white",fg="#258EF5",activebackground="white", activeforeground="#258EF5",font=("Arial",14),padx=0,pady=0, relief="flat")
-signUp.place(x=1350,y=620)
+signUp.place(x=1350,y=650)
 
 #--------------------------leave button---------------------------------------------
 leavebutton=Button(window,text="Leave",command=window.quit,bg="#258EF5",fg="white",activebackground="#258EF5", activeforeground="white",font=("Arial",16),padx=0,pady=0, relief="flat")      #
@@ -98,7 +135,7 @@ leavebutton.place(x=1440, y=800)
 #------------------------contact support -------------------------------------------
 framei = Frame(window, width=200, height=200,relief="ridge")
 framei.place(x=650,y=780)
-icon=PhotoImage(file=r"C:\\Users\\lokmane\\Desktop\\Tkinter_project\\support.png")
+icon=PhotoImage(file=r"C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\support.png")
 button=Button(framei,image=icon,padx=0,pady=0,relief="flat",bg="white")
 button.pack()
 
@@ -109,3 +146,4 @@ button.pack()
 
 
 window.mainloop()
+

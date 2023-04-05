c70d3c3e139cbebacd27992070b8e54e52de1bf7
lokmane101 <lokmaneakkouh10@gmail.com>
2023-04-05 21:14:25 +0100

    g

--- Account.py
+++ Account.py
@@ -1,10 +1,14 @@
 from tkinter import *
 import mysql.connector 
 account=Tk()
-account.attributes('-fullscreen',True)
+account.geometry("1200x720")
 iconsbarr=Frame(account,width=150,height=1440,bg="#15b4ea",)
 iconsbarr.place(x=0,y=0)
-
+def get_user_name():
+    logfile=open("fichierlog.txt",'r')
+    users=logfile.read().split("\n")
+    username=users[-2]
+    return username
 
 #------------------------------------------partie SQL-------------------------------------------------------------------------------------------------------#
 
@@ -30,13 +34,24 @@ paper_button.place(x=25,y=200)
 
 icon=PhotoImage(file=r"C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\support.png")
 button=Button(iconsbarr,image=icon,padx=0,pady=0,relief="flat",bg="#15b4ea",activebackground="#15b4ea")
-button.place(x=25,y=780)
+button.place(x=25,y=620)
 
 person=PhotoImage(file=r"C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\person_icon1.png")
 person_button=Button(iconsbarr,image=person,padx=0,pady=0,relief="flat",bg="#15b4ea",activebackground="#15b4ea")
 person_button.place(x=18,y=300)
+
+
+
+#-------------------------------body----------------------------------------------------------------------------
+
+hello=Label(text="HELLO\t"+get_user_name(),font=("Arial",30),fg="#258EF5")
+hello.place(x=500,y=50)
+
+
+
+
 #--------------------------leave button-------------------------------------------
 leavebutton=Button(account,text="Leave",command=account.quit,bg="#258EF5",fg="white",activebackground="#258EF5", activeforeground="white",font=("Arial",16),padx=0,pady=0, relief="flat")      #
-leavebutton.place(x=1440, y=800)
+leavebutton.place(x=1100, y=650)
 
 account.mainloop()
--- Luncher.py
+++ Luncher.py
@@ -94,45 +94,45 @@ person_icon=ImageTk.PhotoImage(person_icon)
 
 
 
-#----------------------------welcome label--------------------------------------------
+#----------------------------welcome label--------------------------------------------------------------------
 welcome=Label(window,text="ESPACE     ETUDIANT ",font=("Arial",54),bg="white")
 welcome.place(x=435,y=0)
 
-#---------------------------nom label and entry field--------------------------------
+#---------------------------nom label and entry field-------------------------------------------------------
 nom=Label(window,text="Nom d'utilisateur:",font=("Arial",35),bg="white",image=person_icon,compound="left")
 nom.place(x=600,y=150)
 
 nomfield=Entry(window,width=20,font=("Arial",30),relief="flat",bg="#e1f3ff")
 nomfield.place(x=580,y=220)
-#--------------------------password label and entry field----------------------------
+#--------------------------password label and entry field-----------------------------------------------------
 password=Label(window,text="Mot de passe:",font=("Arial",35),bg="white",image=lock_icon,compound="left")
 password.place(x=600,y=280)
 
 passwordfield=Entry(window,width=20,font=("Arial",30),relief="flat",bg="#e1f3ff")
 passwordfield.place(x=580,y=340)
-#-------------------confirm button---------------------------------------------------
+#-------------------confirm button---------------------------------------------------------------------------
 
 confirm_button=Button(window,text="confirm",command=pass_account, bg="#258EF5",fg="white",activebackground="#258EF5", activeforeground="white",font=("Arial",20),padx=0,pady=0, relief="flat",width=27)
 confirm_button.place(x=580,y=450)
 
-#-------------------------forgot password -------------------------------------------
+#-------------------------forgot password --------------------------------------------------------------------
 
 
 forgot_password=Button(window,text=("password forgoten"),command=forgoten_password,bg="white",fg="#258EF5",activebackground="white", activeforeground="#258EF5",font=("Arial",14),padx=0,pady=0, relief="flat")
 forgot_password.place(x=580,y=530)
 
 
-#--------------------------sing up buttom--------------------------------------------
+#--------------------------sing up buttom-----------------------------------------------------------------------
 
 signUp=Button(window,text=("sign up"),command=signup_button,bg="white",fg="#258EF5",activebackground="white", activeforeground="#258EF5",font=("Arial",14),padx=0,pady=0, relief="flat")
 signUp.place(x=940,y=530)
 
-#--------------------------leave button---------------------------------------------
+#--------------------------leave button--------------------------------------------------------------------------
 leavebutton=Button(window,text="Leave",command=window.quit,bg="#258EF5",fg="white",activebackground="#258EF5", activeforeground="white",font=("Arial",16),padx=0,pady=0, relief="flat")      #
 leavebutton.place(x=1100, y=660)
 
 
-#------------------------contact support -------------------------------------------
+#------------------------contact support --------------------------------------------------------------------------
 framei = Frame(window, width=200, height=200,relief="ridge")
 framei.place(x=460,y=640)
 icon=PhotoImage(file=r"C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\support.png")

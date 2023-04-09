da20c785e5970fa535e4571a97777da8315d8b98
lokmane101 <lokmaneakkouh10@gmail.com>
2023-04-09 21:08:42 +0100

    a9a minray9a afkir ijini

--- Account.py
+++ Account.py
@@ -20,7 +20,9 @@ def get_user_second_name():
     cursorr.execute("SELECT prenom from Etudiant where CIN='"+username+"';")
     result=cursorr.fetchone()
     return result[0]
-
+def log_out():
+    account.destroy()
+    subprocess.run(["python","C:\\Users\\lokmane\\Desktop\\Tkinter_project\\Luncher.py"])
 def get_user_first_name():
     logfile=open("fichierlog.txt",'r')
     users=logfile.read().split("\n")
@@ -78,21 +80,22 @@ person_button=Button(iconsbarr,image=person_icon,padx=0,pady=0,relief="flat",bg=
 person_button.place(x=18,y=30)
 
 
-agenda_icon=imporicon("C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\agenda.png",(70,70))
-agenda_icon_button=Button(iconsbarr,bg="#15b4ea",padx=0,pady=0, relief="flat",image=agenda_icon,activebackground="#15b4ea")
-agenda_icon_button.place(x=35,y=140)
+school_icon=imporicon("C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\school.png",(90,90))
+school_icon_button=Button(iconsbarr,image=school_icon,padx=0,pady=0,relief="flat",bg="#15b4ea",activebackground="#15b4ea")
+school_icon_button.place(x=25,y=140)
 
 paper=imporicon("C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\paper1.png",(80,80))
 paper_button=Button(iconsbarr,bg="#15b4ea",padx=0,pady=0, relief="flat",image=paper,activebackground="#15b4ea")
-paper_button.place(x=30,y=240)
+paper_button.place(x=30,y=250)
 
 book_icon=imporicon("C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\book.png",(90,90))
 book_icon_button=Button(iconsbarr,bg="#15b4ea",padx=0,pady=0, relief="flat",image=book_icon,activebackground="#15b4ea")
 book_icon_button.place(x=20,y=350)
 
-school_icon=imporicon("C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\school.png",(90,90))
-school_icon_button=Button(iconsbarr,image=school_icon,padx=0,pady=0,relief="flat",bg="#15b4ea",activebackground="#15b4ea")
-school_icon_button.place(x=25,y=460)
+
+agenda_icon=imporicon("C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\agenda.png",(70,70))
+agenda_icon_button=Button(iconsbarr,bg="#15b4ea",padx=0,pady=0, relief="flat",image=agenda_icon,activebackground="#15b4ea")
+agenda_icon_button.place(x=35,y=460)
 
 support_icon=imporicon("C:\\Users\\lokmane\\Desktop\\Tkinter_project\\icons\\support.png",(70,70))
 button=Button(iconsbarr,image=support_icon,padx=0,pady=0,relief="flat",bg="#15b4ea",activebackground="#15b4ea")
@@ -103,31 +106,37 @@ button.place(x=30,y=630)
 hello=Label(text="HELLO\t"+get_user_second_name(),font=("Arial",30),fg="#258EF5",bg="white")
 hello.place(x=500,y=30)
 #------------------------------personal data frame------------------------------------------------------------------
-body_frame=Frame(account,bg="#E0E6E7",width=1000,height=530,relief="flat")
+body_frame=Frame(account,bg="#B5EFFF",width=1000,height=530,relief="flat")
 body_frame.place(x=170,y=100)
 #-------------------- personal picture import ----------------------------------------------------------
-photo=imporicon(get_user_picture(),(150,150))
+photo=imporicon(get_user_picture(),(200,200))
 photo_label=Label(body_frame,image=photo)
 photo_label.place(x=0,y=0)
 #----------------------show other data ----------------------------------------------------------------------
-CIN_label=Label(body_frame,text="CIN:",bg="#E0E6E7",fg="black",font=("Arila",20))
-CIN_label.place(x=180,y=20)
-CIN_label2=Label(body_frame,text=get_user_CIN(),bg="#E0E6E7",fg="black",font=("Arila",20))
-CIN_label2.place(x=400,y=20)
-CNE_label=Label(body_frame,text="CNE:",bg="#E0E6E7",fg="black",font=("Arila",20))
-CNE_label.place(x=180,y=50)
-CNE_label2=Label(body_frame,text=get_user_CNE(),bg="#E0E6E7",fg="black",font=("Arila",20))
-CNE_label2.place(x=400,y=50)
-firstName_label=Label(body_frame,text="NOM:",bg="#E0E6E7",fg="black",font=("Arila",20))
-firstName_label.place(x=180,y=80)
-firstName_label2=Label(body_frame,text=get_user_first_name(),bg="#E0E6E7",fg="black",font=("Arila",20))
-firstName_label2.place(x=400,y=80)
-second_Name_label=Label(body_frame,text="PRENOM:",bg="#E0E6E7",fg="black",font=("Arila",20))
-second_Name_label.place(x=180,y=110)
-second_Name_label2=Label(body_frame,text=get_user_second_name(),bg="#E0E6E7",fg="black",font=("Arila",20))
-second_Name_label2.place(x=400,y=110)
-#--------------------------leave button-------------------------------------------
-leavebutton=Button(account,text="Leave",command=account.quit,bg="#258EF5",fg="white",activebackground="#258EF5", activeforeground="white",font=("Arial",16),padx=0,pady=0, relief="flat")      #
-leavebutton.place(x=1100, y=650)
+CIN_label=Label(body_frame,text="CIN:",bg="#B5EFFF",fg="black",font=("Arila",35))
+CIN_label.place(x=220,y=240)
+CIN_label2=Label(body_frame,text=get_user_CIN(),bg="#B5EFFF",fg="black",font=("Arila",35))
+CIN_label2.place(x=500,y=240)
 
+CNE_label=Label(body_frame,text="CNE:",bg="#B5EFFF",fg="black",font=("Arila",35))
+CNE_label.place(x=220,y=310)
+CNE_label2=Label(body_frame,text=get_user_CNE(),bg="#B5EFFF",fg="black",font=("Arila",35))
+CNE_label2.place(x=500,y=310)
+
+firstName_label=Label(body_frame,text="NOM:",bg="#B5EFFF",fg="black",font=("Arila",35))
+firstName_label.place(x=220,y=380)
+firstName_label2=Label(body_frame,text=get_user_first_name(),bg="#B5EFFF",fg="black",font=("Arila",35))
+firstName_label2.place(x=500,y=380)
+
+second_Name_label=Label(body_frame,text="PRENOM:",bg="#B5EFFF",fg="black",font=("Arila",35))
+second_Name_label.place(x=220,y=450)
+second_Name_label2=Label(body_frame,text=get_user_second_name(),bg="#B5EFFF",fg="black",font=("Arila",35))
+second_Name_label2.place(x=500,y=450)
+
+#--------------------------leave button-------------------------------------------
+leavebutton=Button(account,text="Quitter",command=account.quit,bg="#258EF5",fg="white",activebackground="#258EF5", activeforeground="white",font=("Arial",16),padx=0,pady=0, relief="flat")      #
+leavebutton.place(x=1100, y=660)
+#--------------------------logout button--------------------------------------------
+logout=Button(account,text="log out",command=log_out,bg="#258EF5",fg="white",activebackground="#258EF5", activeforeground="white",font=("Arial",16),padx=0,pady=0, relief="flat")      #
+logout.place(x=180, y=660)
 account.mainloop()

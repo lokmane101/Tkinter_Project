81bd6bf97921f86f099ad6d4b88dee6b3d827f91
bentouhami <bentouhamimohamedrida@gmail.com>
2023-04-27 03:59:44 +0100

    zid waahaa

--- Account.py
+++ Account.py
@@ -1,24 +1,21 @@
 from tkinter import *
-from tkinter import *
 import mysql.connector 
 import subprocess
 from io import BytesIO
 from PIL import Image,ImageTk
 import os
 import webbrowser
-from tkinter import messagebox
-
+def emploi():
+    url = 'https://drive.google.com/file/d/1Nz6Mu8ZliD2G_Hn4mM5zV0aJuBbOOar5/view?usp=sharing'
+    webbrowser.open_new_tab(url)
 current_path=os.getcwd()
 account=Tk()
 account.geometry("1200x720")
 account.config(bg="white")
 iconsbarr=Frame(account,width=200,height=1440,bg="#15b4ea",)
 iconsbarr.place(x=0,y=0)
-logfile=open("fichierlog.txt",'r')
-users=logfile.read().split("\n")
-username=users[-2]
 
-def info_personel():
+def personel():
     personel=Tk()
     personel.geometry=("1200x720")
     personel.config(bg="white")
@@ -27,15 +24,15 @@ def get_username():
     users=logfile.read().split("\n")
     username=users[-2]
     cursorr.execute("SELECT nom from Etudiant where Cne='"+username+"';")
-    result=cursorr.fetchone()
-    return result[0]
+    result=cursorr.fetchall()
+    return result[0][0]
 def get_user_second_name():
     logfile=open("fichierlog.txt",'r')
     users=logfile.read().split("\n")
     username=users[-2]
     cursorr.execute("SELECT prenom from Etudiant where Cne='"+username+"';")
-    result=cursorr.fetchone()
-    return result[0]
+    result=cursorr.fetchall()
+    return result[0][0]
 def log_out():
     account.destroy()
     subprocess.run(["python",current_path+"\\Luncher.py"])
@@ -43,24 +40,26 @@ def get_user_first_name():
     logfile=open("fichierlog.txt",'r')
     users=logfile.read().split("\n")
     username=users[-2]
+    print(username)
     cursorr.execute("SELECT nom from Etudiant where Cne='"+username+"';")
-    result=cursorr.fetchone()
-    return result[0]
+    result=cursorr.fetchall()
+    return result[0][0]
 def get_filiere():
     logfile=open("fichierlog.txt",'r')
     users=logfile.read().split("\n")
     username=users[-2]
     cursorr.execute("SELECT filière from Etudiant where Cne='"+username+"';")
-    result=cursorr.fetchone()
-    return result[0]
+    result=cursorr.fetchall()
+    return result[0][0]
 
 def get_user_picture():
     logfile=open("fichierlog.txt",'r')
     users=logfile.read().split("\n")
     username=users[-2]
+    print(username)
     cursorr.execute("SELECT image from Etudiant where Cne='"+username+"';")
-    result=cursorr.fetchone()
-    photo_in_byte=result[0]
+    result=cursorr.fetchall()
+    photo_in_byte=result[0][0]
     picture=imporicon(BytesIO(photo_in_byte),(200,200))
     return picture
 
@@ -69,8 +68,8 @@ def get_user_CNE():
     users=logfile.read().split("\n")
     username=users[-2]
     cursorr.execute("SELECT CNE from Etudiant where Cne='"+username+"';")
-    result=cursorr.fetchone()
-    cne=result[0]
+    result=cursorr.fetchall()
+    cne=result[0][0]
     return cne
 
 def get_user_CIN():
@@ -78,8 +77,8 @@ def get_user_CIN():
     users=logfile.read().split("\n")
     username=users[-2]
     cursorr.execute("SELECT CIN from Etudiant where Cne='"+username+"';")
-    result=cursorr.fetchone()
-    cin=result[0]
+    result=cursorr.fetchall()
+    cin=result[0][0]
     return cin
 
 def imporicon(path,size_tuple):
@@ -87,52 +86,6 @@ def imporicon(path,size_tuple):
     icon=icon.resize(size_tuple, Image.ANTIALIAS)
     icon=ImageTk.PhotoImage(icon)
     return icon
-
-def emploi():
-     
-    cursorr.execute("SELECT filiere from Etudiant where cne='"+username+"';")
-    result=cursorr.fetchone()
-    filier=result[0]
-    cursorr.execute("SELECT link from emploidutemps where filier='"+filier+"';")
-    print(filier)
-    url =cursorr.fetchone() 
-    link=url[0]
-    print(link)
-    webbrowser.open_new_tab(link)
-def get_password():
-     
-    cursorr.execute("SELECT mot_de_passe from Etudiant where cne='"+username+"';")
-    result=cursorr.fetchone()
-    mot_de_passe=result[0]
-    return mot_de_passe
-def get_téléphone():
-     
-    cursorr.execute("SELECT téléphone from Etudiant where cne='"+username+"';")
-    result=cursorr.fetchone()
-    téléphone=result[0]
-    return téléphone
-
-def get_email():
-     
-    cursorr.execute("SELECT email from Etudiant where cne='"+username+"';")
-    result=cursorr.fetchone()
-    email=result[0]
-    return email
-
-def get_date_de_naissance():
-     
-    cursorr.execute("SELECT date_de_naissance from Etudiant where cne='"+username+"';")
-    result=cursorr.fetchone()
-    date_de_naissance=result[0]
-    return date_de_naissance
-
-
-
-
-
-
-def support():
-    messagebox.showinfo(title="SUPPORT", message="CONTACTER UN DES ADMINS :\n\n\nAFKIR MOHAMED \t email\n\n\nAKKOUH LOKMANE \t lokmaneakkouh10@gmail.com\n\n\n BEN TOUHAMI MOHAMED RIDA \t email")
 
 #------------------------------------------partie SQL-------------------------------------------------------------------------------------------------------#
 
@@ -147,7 +100,7 @@ cursorr=database.cursor()
 #-----------------------------import icons----------------------------------------
 
 person_icon=imporicon(current_path+"\\icons\\person_icon1.png",(80,80))
-person_button=Button(iconsbarr,text="PROFIL",command=info_personel,image=person_icon,compound="top" ,font=("Louis George Cafe",20),padx=0,pady=0,relief="flat",bg="#15b4ea",activebackground="#15b4ea",fg="white",activeforeground="white",highlightcolor="white")
+person_button=Button(iconsbarr,text="PROFIL",image=person_icon,compound="top" ,font=("Louis George Cafe",20),padx=0,pady=0,relief="flat",bg="#15b4ea",activebackground="#15b4ea",fg="white",activeforeground="white",highlightcolor="white")
 person_button.place(x=18,y=0)
 
 
@@ -169,7 +122,7 @@ agenda_icon_button=Button(iconsbarr,bg="#15b4ea",padx=0,pady=0, relief="flat",co
 agenda_icon_button.place(x=10,y=460)
 
 support_icon=imporicon(current_path+"\\icons\\support.png",(70,70))
-button=Button(iconsbarr,command=support,image=support_icon,padx=0,pady=0,relief="flat",bg="#15b4ea",activebackground="#15b4ea")
+button=Button(iconsbarr,image=support_icon,padx=0,pady=0,relief="flat",bg="#15b4ea",activebackground="#15b4ea")
 button.place(x=30,y=630)
 
 
@@ -184,11 +137,9 @@ photo=get_user_picture()
 photo_label=Label(body_frame,image=photo)
 photo_label.place(x=0,y=0)
 #----------------------show other data ----------------------------------------------------------------------
-
-
 username_label=Label(body_frame,text="NOM D'UTILISATEUR:",bg="#B5EFFF",fg="#0073e6",font=("Arila",35))
 username_label.place(x=220,y=0)
-username_label2=Label(body_frame,text=get_user_CNE(),bg="#B5EFFF",fg="white",font=("Arila",35))
+username_label2=Label(body_frame,text=get_username(),bg="#B5EFFF",fg="white",font=("Arila",35))
 username_label2.place(x=700,y=50)
 
 firstName_label=Label(body_frame,text="NOM:",bg="#B5EFFF",fg="#0073e6",font=("Arila",35))
@@ -210,8 +161,6 @@ filier_label=Label(body_frame,text="FILIERE:",bg="#B5EFFF",fg="#0073e6",font=("A
 filier_label.place(x=220,y=400)
 filier_label2=Label(body_frame,text=get_filiere(),bg="#B5EFFF",fg="white",font=("Arila",35))
 filier_label2.place(x=700,y=450)
-
-
 
 #--------------------------leave button-------------------------------------------
 leavebutton=Button(account,text="Quitter",command=account.quit,bg="#258EF5",fg="white",activebackground="#258EF5", activeforeground="white",font=("Arial",16),padx=0,pady=0, relief="flat")      #

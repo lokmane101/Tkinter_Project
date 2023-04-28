49f95fc1d02569368b36f016c856243ca953e427
bentouhami <bentouhamimohamedrida@gmail.com>
2023-04-28 04:29:48 +0100

    petit changement

--- Description_filière.py
+++ Description_filière.py
@@ -37,7 +37,7 @@ db=DataBase()
 #_____________________________________________creation de la fenêtre_________________________________________________#
 account=Tk()
 account.geometry("1200x720")
-account.config(bg="white")
+account.config(bg="#FFEBCD")
 
 
 
@@ -45,7 +45,7 @@ account.config(bg="white")
 
 
 #______________________________creation du frame des icons_______________________________________#
-iconsbarr=Frame(account,width=150,height=1440,bg="#15b4ea")
+iconsbarr=Frame(account,width=155,height=1440,bg="#15b4ea")
 iconsbarr.place(x=0,y=0)
 
 #-----------------------------import icons----------------------------------------
@@ -56,12 +56,12 @@ person_button.place(x=18,y=0)
 
 
 school_icon=imporicon(current_path+"\\icons\\school.png",(80,80))
-school_icon_button=Button(iconsbarr,image=school_icon,padx=0,pady=0,relief="flat",bg="#15b4ea",activebackground="#15b4ea",compound="top" ,font=("Louis George Cafe",20),text="text ici",fg="white",activeforeground="white",command=go_to_accueil)
-school_icon_button.place(x=25,y=115)
+school_icon_button=Button(iconsbarr,image=school_icon,padx=0,pady=0,relief="flat",bg="#15b4ea",activebackground="#15b4ea",compound="top" ,font=("Louis George Cafe",20),text="ACCUEIL",fg="white",activeforeground="white",command=go_to_accueil)
+school_icon_button.place(x=10,y=115)
 
 paper=imporicon(current_path+"\\icons\\paper1.png",(70,70))
-paper_button=Button(iconsbarr,bg="#15b4ea",padx=0,pady=0, relief="flat",image=paper,activebackground="#15b4ea",compound="top" ,font=("Louis George Cafe",20),text="text ici",fg="white",activeforeground="white")
-paper_button.place(x=24,y=230)
+paper_button=Button(iconsbarr,bg="#15b4ea",padx=0,pady=0, relief="flat",image=paper,activebackground="#15b4ea",compound="top" ,font=("Louis George Cafe",20),text="DESCRIPT°",fg="white",activeforeground="white")
+paper_button.place(x=0,y=230)
 
 book_icon=imporicon(current_path+"\\icons\\book.png",(70,70))
 book_icon_button=Button(iconsbarr,bg="#15b4ea",padx=0,pady=0, relief="flat",image=book_icon,activebackground="#15b4ea",compound="top" ,font=("Louis George Cafe",18),text="COURS",fg="white",activeforeground="white",command=go_to_cours)
@@ -77,21 +77,21 @@ button=Button(iconsbarr,image=support_icon,padx=0,pady=0,relief="flat",bg="#15b4
 button.place(x=30,y=630)
 #-------------frame (Nom filière et nom d'étudiant)-------------- 
 
-Student_frame=Frame(account,bg="#5CECAE")
+Student_frame=Frame(account,bg="#FFEBCD")
 Student_frame.place(x=220,y=10, height=150,width=1100)
 
 
 nom,prenom,filière=db.getStudent()
 
-Student_fil=Label(Student_frame,text=filière,font=("LEMONMILK-Medium",50),fg="white",bg="#5CECAE")
+Student_fil=Label(Student_frame,text=filière,font=("LEMONMILK-Medium",50),fg="#A92B6F",bg="#FFEBCD")
 Student_fil.place(x=170,y=25)
 
-Student_nom_prenom=Label(Student_frame,text="Mr(s)."+nom+" "+prenom,font=("LEMONMILK-Medium",15),fg="white",bg="#5CECAE")
+Student_nom_prenom=Label(Student_frame,text="Mr(s)."+nom+" "+prenom,font=("LEMONMILK-Medium",15),fg="#A92B6F",bg="#FFEBCD")
 Student_nom_prenom.place(x=780,y=120)
 
 #-------------------frame (des professeurs)---------------------
 
-modules_frame=Frame(account,bg="#5CECAE" )
+modules_frame=Frame(account,bg="#FFEBCD" )
 modules_frame.place(x=160,y=180, height=550,width=1200)
 
 # def create_listBox()
